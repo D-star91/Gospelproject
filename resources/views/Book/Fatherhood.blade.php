df5bb0e4b16693a8f17e67c9945345945e289b64
Daniel Htoo <74122831+D-star91@users.noreply.github.com>
2021-04-08 09:35:43 +0630

    book

--- resources/views/Book/Fatherhood.blade.php
+++ resources/views/Book/Fatherhood.blade.php
@@ -2,14 +2,15 @@
 
    <div class="book-bg-pg">
        <div class="container">
-           <div class="row ">
-               <div class="col-md-2">
+           <div class="row justify-content-center">
+               <div class="col-md-8">
                     <div class="book-page-title">
                         <h3 class="book-font-title">ဖခင်ဟူသည်</h3>
                     </div>
                </div>
            </div>
-           <div class="row justify-content-center">
+           <div class="row justify-content-center mb-5">
+               <div class="col-md-2">  </div>
                <div class="col-md-8">
                     <div class="page-pg">
                         <div class="tab-content" id="nav-tabContent">
@@ -21,12 +22,33 @@
                                 </div>
                             </div>
                             <div class="tab-pane fade" id="list-page2" role="tabpanel" aria-labelledby="list-page-2">2</div>
-                            <div class="tab-pane fade" id="list-page3" role="tabpanel" aria-labelledby="list-page-3">3</div>
+                            <div class="tab-pane fade" id="list-page3" role="tabpanel" aria-labelledby="list-page-3">
+                                <div class="fh-menu">
+                                    <h3 class="fh-menu-tit">မာတိကာ</h3>
+                                    <ul class="fh-menu-ul">
+                                        <li class="fh-menu-li">
+                                            ဘုရားသခင်၏ ဖခင်အရာဖြစ်ခြင်း
+                                            <ul>
+                                                <li class="fh-menu-li1">ထပ်တူထပ်မျှဖြစ်ခြင်း</li>
+                                                <li class="fh-menu-li1">မိမိကိုယ်ကို တန်ဖိုးထားခြင်း</li>
+                                                <li class="fh-menu-li1">ယုံကြည်စိတ်ချခြင်း</li>
+                                            </ul>
+                                        </li>
+                                        <li class="fh-menu-li">ပုထုဇဉ်လူသား၏ဖခင်ဖြစ်ခြင်း</li>
+                                        <li class="fh-menu-li">ဖခင်သည် ယဇ်ပုရောဟိတ်</li>
+                                        <li class="fh-menu-li">ဖခင်သည် ပရောဖက်</li>
+                                        <li class="fh-menu-li">ဖခင်သည် ရှင်ဘုရင်</li>
+                                        <li class="fh-menu-li"> ဖခင်ဝတ္တရားပျက်ကွက်သောအခါ</li>
+                                        <li class="fh-menu-li">စာရေးသူ၏ အတ္ထုပ္ပတ္တိ</li>
+                                    </ul>
+                                </div>
+                                
+                            </div>
                             <div class="tab-pane fade" id="list-page4" role="tabpanel" aria-labelledby="list-page-4">
-                                <div class="fh-page"><br> <br><br>
-                                    <h3 class="title-fh2">နိဒါန်း</h3><br><br>
-                                    <hr><br><br>
-                                    <p class="book-p1-font"><span class="book-tab">သမ္မာကျမ်း၌ဖော်ပြသော ကြီးမြတ်သည့်အရာများထဲက ဖခင်အရာသည်လည်း အပါအဝင်ဖြစ်သည်။ ဤအကြောင်းကို ခရစ်ယာန်အများစုက လျစ်လျူရှုထားကြသည်။</span></p>
+                                <div class="fh-page">
+                                    <h3 class="title-fh2">နိဒါန်း</h3>
+                                    <hr>
+                                    <p class="book-p1-font fh-enter-br"><span class="book-tab">သမ္မာကျမ်း၌ဖော်ပြသော ကြီးမြတ်သည့်အရာများထဲက ဖခင်အရာသည်လည်း အပါအဝင်ဖြစ်သည်။ ဤအကြောင်းကို ခရစ်ယာန်အများစုက လျစ်လျူရှုထားကြသည်။</span></p>
                                     <p class="book-p1-font"><span class="book-tab">ဖခင်အရာဖြစ်ခြင်းသည် သမ္မာကျမ်းထဲတွင် လေ့လာရုံသာမဟုတ်ဘဲ ကျွန်ုပ်ကိုယ်တိုင်လည်းဖခင်တစ် ဦးအဖြစ် ကျွန်တော့် ပထမဇနီး လေးဒီယာနှင့် မွေးစားသားသမီးကိုးယောက် ပြုစုပျိုးထောင်ခဲ့ကြပါသည်။ ပထမဇနီး ကွယ်လွန်ပြီးနောက် ဒုတိယဇနီး ရုသနှင့် မူလစုစုပေါင်းမိသားစုထဲသို့ သုံးယောက်ထပ်ပေါင်းကြပါ သည်။ ထို့ကြောင့် စုပေါင်းလိုက်သည့်အခါ ကျွန်ုပ်သည် လူတစ်ကျိပ်နှစ်ယောက်တို့၏ ဖခင်အရိုက်အရာ၌ တည်ရှိနေပါသည်။ ယခုအခါ အရွယ်ရောက်လာကြပြီဖြစ်၍ အိမ်ထောင်အသီးသီးနှင့် ဖြစ်နေပါပြီ။</span></p>
                                 </div>
                             </div>
@@ -52,42 +74,42 @@
                         </div>
                     </div>
                </div>
+               <div class="col-md-2">
+                <div class="book-page-listbtn">
+                    <h3 class="list-font-fh">စာမျက်နှာများ</h3>
+                    <div class="btn-group " role="group" aria-label="First group">
+                        <div class="list-group d-inline" id="list-tab" role="tablist">
+                            <a class="btn btn-danger active" id="list-page-1" data-bs-toggle="list" href="#list-page1" role="tab" aria-controls="page1">1</a>
+                            <a class="btn btn-danger mt-1" id="list-page-2" data-bs-toggle="list" href="#list-page2" role="tab" aria-controls="page2">2</a>
+                            <a class="btn btn-danger mt-1" id="list-page-3" data-bs-toggle="list" href="#list-page3" role="tab" aria-controls="page3">3</a>
+                            <a class="btn btn-danger mt-1" id="list-page-4" data-bs-toggle="list" href="#list-page4" role="tab" aria-controls="page4">4</a>    
+                            <a class="btn btn-danger mt-1" id="list-page-5" data-bs-toggle="list" href="#list-page5" role="tab" aria-controls="page5">5</a>
+                            <a class="btn btn-danger mt-1" id="list-page-6" data-bs-toggle="list" href="#list-page6" role="tab" aria-controls="page6">6</a>
+                            <a class="btn btn-danger mt-1" id="list-page-7" data-bs-toggle="list" href="#list-page7" role="tab" aria-controls="page7">7</a>
+                            <a class="btn btn-danger mt-1" id="list-page-8" data-bs-toggle="list" href="#list-page8" role="tab" aria-controls="page8">8</a>
+                            <a class="btn btn-danger mt-1" id="list-page-9" data-bs-toggle="list" href="#list-page9" role="tab" aria-controls="page9">9</a>
+                            <a class="btn btn-danger mt-1" id="list-page-10" data-bs-toggle="list" href="#list-page10" role="tab" aria-controls="page10">10</a>
+                            <a class="btn btn-danger mt-1" id="list-page-11" data-bs-toggle="list" href="#list-page11" role="tab" aria-controls="page11">11</a>    
+                            <a class="btn btn-danger mt-1" id="list-page-12" data-bs-toggle="list" href="#list-page12" role="tab" aria-controls="page12">12</a>
+                            <a class="btn btn-danger mt-1" id="list-page-13" data-bs-toggle="list" href="#list-page13" role="tab" aria-controls="page13">13</a>
+                            <a class="btn btn-danger mt-1" id="list-page-14" data-bs-toggle="list" href="#list-page14" role="tab" aria-controls="page14">14</a>
+                            <a class="btn btn-danger mt-1" id="list-page-15" data-bs-toggle="list" href="#list-page15" role="tab" aria-controls="page15">15</a>
+                            <a class="btn btn-danger mt-1" id="list-page-16" data-bs-toggle="list" href="#list-page16" role="tab" aria-controls="page16">16</a>
+                            <a class="btn btn-danger mt-1" id="list-page-17" data-bs-toggle="list" href="#list-page17" role="tab" aria-controls="page17">17</a>
+                            <a class="btn btn-danger mt-2" id="list-page-18" data-bs-toggle="list" href="#list-page18" role="tab" aria-controls="page18">18</a>    
+                            <a class="btn btn-danger mt-2" id="list-page-19" data-bs-toggle="list" href="#list-page19" role="tab" aria-controls="page19">19</a>
+                            <a class="btn btn-danger mt-2" id="list-page-20" data-bs-toggle="list" href="#list-page20" role="tab" aria-controls="page20">20</a>
+                            <a class="btn btn-danger mt-2" id="list-page-21" data-bs-toggle="list" href="#list-page21" role="tab" aria-controls="page21">21</a>
+                            <a class="btn btn-danger mt-2" id="list-page-22" data-bs-toggle="list" href="#list-page22" role="tab" aria-controls="page22">22</a>
+                            <a class="btn btn-danger mt-2" id="list-page-23" data-bs-toggle="list" href="#list-page23" role="tab" aria-controls="page23">23</a>
+                            
+                        </div>
+                    </div>
+                </div>
+               </div>
            </div>
            <div class="row justify-content-center">
                <div class="col-md-8">
-                    <div class="book-page-listbtn">
-                        <div class="btn-group " role="group" aria-label="First group">
-                            <div class="list-group d-inline" id="list-tab" role="tablist">
-                                <a class="btn btn-danger active" id="list-page-1" data-bs-toggle="list" href="#list-page1" role="tab" aria-controls="page1">1</a>
-                                <a class="btn btn-danger" id="list-page-2" data-bs-toggle="list" href="#list-page2" role="tab" aria-controls="page2">2</a>
-                                <a class="btn btn-danger" id="list-page-3" data-bs-toggle="list" href="#list-page3" role="tab" aria-controls="page3">3</a>
-                                <a class="btn btn-danger" id="list-page-4" data-bs-toggle="list" href="#list-page4" role="tab" aria-controls="page4">4</a>    
-                                <a class="btn btn-danger" id="list-page-5" data-bs-toggle="list" href="#list-page5" role="tab" aria-controls="page5">5</a>
-                                <a class="btn btn-danger" id="list-page-6" data-bs-toggle="list" href="#list-page6" role="tab" aria-controls="page6">6</a>
-                                <a class="btn btn-danger" id="list-page-7" data-bs-toggle="list" href="#list-page7" role="tab" aria-controls="page7">7</a>
-                                <a class="btn btn-danger" id="list-page-8" data-bs-toggle="list" href="#list-page8" role="tab" aria-controls="page8">8</a>
-                                <a class="btn btn-danger" id="list-page-9" data-bs-toggle="list" href="#list-page9" role="tab" aria-controls="page9">9</a>
-                                <a class="btn btn-danger" id="list-page-10" data-bs-toggle="list" href="#list-page10" role="tab" aria-controls="page10">10</a>
-                                <a class="btn btn-danger" id="list-page-11" data-bs-toggle="list" href="#list-page11" role="tab" aria-controls="page11">11</a>    
-                                <a class="btn btn-danger" id="list-page-12" data-bs-toggle="list" href="#list-page12" role="tab" aria-controls="page12">12</a>
-                                <a class="btn btn-danger" id="list-page-13" data-bs-toggle="list" href="#list-page13" role="tab" aria-controls="page13">13</a>
-                                <a class="btn btn-danger" id="list-page-14" data-bs-toggle="list" href="#list-page14" role="tab" aria-controls="page14">14</a>
-                                <a class="btn btn-danger" id="list-page-15" data-bs-toggle="list" href="#list-page15" role="tab" aria-controls="page15">15</a>
-                                <a class="btn btn-danger" id="list-page-16" data-bs-toggle="list" href="#list-page16" role="tab" aria-controls="page16">16</a>
-                                <a class="btn btn-danger mt-2" id="list-page-17" data-bs-toggle="list" href="#list-page17" role="tab" aria-controls="page17">17</a>
-                                <a class="btn btn-danger mt-2" id="list-page-18" data-bs-toggle="list" href="#list-page18" role="tab" aria-controls="page18">18</a>    
-                                <a class="btn btn-danger mt-2" id="list-page-19" data-bs-toggle="list" href="#list-page19" role="tab" aria-controls="page19">19</a>
-                                <a class="btn btn-danger mt-2" id="list-page-20" data-bs-toggle="list" href="#list-page20" role="tab" aria-controls="page20">20</a>
-                                <a class="btn btn-danger mt-2" id="list-page-21" data-bs-toggle="list" href="#list-page21" role="tab" aria-controls="page21">21</a>
-                                <a class="btn btn-danger mt-2" id="list-page-22" data-bs-toggle="list" href="#list-page22" role="tab" aria-controls="page22">22</a>
-                                <a class="btn btn-danger mt-2" id="list-page-23" data-bs-toggle="list" href="#list-page23" role="tab" aria-controls="page23">23</a>
-                                
-                            </div>
-                            <div class="list-group ms-1" id="list-tab" role="tablist">
-                                    
-                            </div>
-                        </div>
-                    </div>
                     
                 
                </div>
